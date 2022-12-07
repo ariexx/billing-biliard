48f6023d37f3395db7baf076a5fe9be49ef4f113
Muhammad Arief <mgi.official1@gmail.com>
2022-12-08 02:46:51 +0700

    fix web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    if(auth()->check()){
+        return redirect()->route('home');
+    }else{
+        return view('auth.login');
+    }
 });
 Auth::routes(['register' => false, 'reset' => false, 'verify' => false]);
 
